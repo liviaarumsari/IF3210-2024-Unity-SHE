f7c8a037bfd470a3ab646e158a1c6bdb425d38ad
chaerla <chelgchen@gmail.com>
2024-05-09 18:43:49 +0700

    fix: comment orb

--- Assets/Scripts/Orb/OrbIncreaseDamage.cs
+++ Assets/Scripts/Orb/OrbIncreaseDamage.cs
@@ -1,55 +1,55 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-namespace Nightmare
-{
-    public class OrbIncreaseDamage : MonoBehaviour
-    {
-        GameObject player;
-        PlayerShooting playerShooting;
-        float multiplier = 0.1f;
-        bool playerInRange;
+// namespace Nightmare
+// {
+//     public class OrbIncreaseDamage : MonoBehaviour
+//     {
+//         GameObject player;
+//         PlayerShooting playerShooting;
+//         float multiplier = 0.1f;
+//         bool playerInRange;
 
-        void Awake()
-        {
-            player = GameObject.FindGameObjectWithTag("Player");
-            playerShooting = player.GetComponent<PlayerShooting>();
-        }
-        void ApplyOrbEffect()
-        {
-            if ((playerShooting.damagePerShot / playerShooting.baseDamage) < 2.5)
-            {
-                playerShooting.damagePerShot += (multiplier * playerShooting.baseDamage);
-            }
-            else
-            {
-                // display maximum orb number excited
-            }
-        }
+//         void Awake()
+//         {
+//             player = GameObject.FindGameObjectWithTag("Player");
+//             playerShooting = player.GetComponent<PlayerShooting>();
+//         }
+//         void ApplyOrbEffect()
+//         {
+//             if ((playerShooting.damagePerShot / playerShooting.baseDamage) < 2.5)
+//             {
+//                 playerShooting.damagePerShot += (multiplier * playerShooting.baseDamage);
+//             }
+//             else
+//             {
+//                 // display maximum orb number excited
+//             }
+//         }
 
-        void OnTriggerEnter(Collider other)
-        {
-            if (other.gameObject == player)
-            {
-                playerInRange = true;
-            }
-        }
+//         void OnTriggerEnter(Collider other)
+//         {
+//             if (other.gameObject == player)
+//             {
+//                 playerInRange = true;
+//             }
+//         }
 
-        // Start is called before the first frame update
-        void Start()
-        {
+//         // Start is called before the first frame update
+//         void Start()
+//         {
 
-        }
+//         }
 
-        // Update is called once per frame
-        void Update()
-        {
-            if (playerInRange)
-            {
-                ApplyOrbEffect();
-            }
-        }
-    }
+//         // Update is called once per frame
+//         void Update()
+//         {
+//             if (playerInRange)
+//             {
+//                 ApplyOrbEffect();
+//             }
+//         }
+//     }
 
-}
+// }
--- Assets/Scripts/Orb/OrbIncreaseSpeed.cs
+++ Assets/Scripts/Orb/OrbIncreaseSpeed.cs
@@ -1,62 +1,62 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-namespace Nightmare
-{
-    public class OrbIncreaseSpeed : MonoBehaviour
-    {
-        GameObject player;
-        PlayerMovement playerMovement;
-        bool playerInRange;
-        float multiplier = 0.2f;
-        public float orbEffectTime = 15f;
-        float timer;
-        bool orbTaken;
+// namespace Nightmare
+// {
+//     public class OrbIncreaseSpeed : MonoBehaviour
+//     {
+//         GameObject player;
+//         PlayerMovement playerMovement;
+//         bool playerInRange;
+//         float multiplier = 0.2f;
+//         public float orbEffectTime = 15f;
+//         float timer;
+//         bool orbTaken;
 
-        void Awake()
-        {
-            player = GameObject.FindGameObjectWithTag("Player");
-            playerMovement = player.GetComponent<PlayerMovement>();
-        }
+//         void Awake()
+//         {
+//             player = GameObject.FindGameObjectWithTag("Player");
+//             playerMovement = player.GetComponent<PlayerMovement>();
+//         }
 
-        void OnTriggerEnter(Collider other)
-        {
-            if (other.gameObject == player)
-            {
-                playerInRange = true;
-            }
-        }
+//         void OnTriggerEnter(Collider other)
+//         {
+//             if (other.gameObject == player)
+//             {
+//                 playerInRange = true;
+//             }
+//         }
 
-        void ApplyOrbEffect()
-        {
-            playerMovement.speed += (multiplier * playerMovement.baseSpeed);
-            orbTaken = true;
-        }
+//         void ApplyOrbEffect()
+//         {
+//             playerMovement.speed += (multiplier * playerMovement.baseSpeed);
+//             orbTaken = true;
+//         }
 
-        void RemoveOrbEffect()
-        {
-            playerMovement.speed -= (multiplier * playerMovement.baseSpeed);
-        }
+//         void RemoveOrbEffect()
+//         {
+//             playerMovement.speed -= (multiplier * playerMovement.baseSpeed);
+//         }
 
-        // Update is called once per frame
-        void Update()
-        {
-            if (playerInRange)
-            {
-                ApplyOrbEffect();
-            }
+//         // Update is called once per frame
+//         void Update()
+//         {
+//             if (playerInRange)
+//             {
+//                 ApplyOrbEffect();
+//             }
 
-            if (orbTaken)
-            {
-                timer += Time.deltaTime;
-            }
+//             if (orbTaken)
+//             {
+//                 timer += Time.deltaTime;
+//             }
 
-            if (timer > orbEffectTime)
-            {
-                RemoveOrbEffect();
-            }
-        }
-    }
-}
+//             if (timer > orbEffectTime)
+//             {
+//                 RemoveOrbEffect();
+//             }
+//         }
+//     }
+// }
 
